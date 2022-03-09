ab7b425edf650a945d0a53810ed15f8fd52b3fa1
unknown <gabrielarrudalima@gmail.com>
2022-03-08 23:42:17 -0300

    CRUD pronto

---
+++ RestWithASPNETUdemy/Services/Implementations/IPersonService.cs
@@ -0,0 +1,15 @@
+﻿namespace RestWithASPNETUdemy.Services;
+using RestWithASPNETUdemy.Model;
+
+    public interface IPersonService
+    {
+
+        Person Create(Person person);
+        Person FindBYID(long id);
+        List<Person> FindAll();
+        Person Update(Person person);
+        void Delete(long id);
+
+
+    }
+
